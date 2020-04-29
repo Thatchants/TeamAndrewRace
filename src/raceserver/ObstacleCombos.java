c046d9ce197ef3ded15c63ea858d35e25eee8164
Thatchants <andrew@bookfamily.com>
2020-04-28 19:38:03 -0700

    obstacle packs

--- src/raceserver/ObstacleCombos.java
+++ src/raceserver/ObstacleCombos.java
@@ -3,6 +3,7 @@ package raceserver;
 public class ObstacleCombos {
     public static final String[] combo1 = new String[]{
             "490", "455", "false",
-            "490", "425", "true"
+            "490", "425", "true",
+            "525", "425", "false"
     };
 }
